1e6ef937e58b19745269086ee83997740d36bba4
Anton Konopelkin <anti.kon.40@yandex.ru>
2023-11-08 07:50:05 +0300

    configured page navigation
    Added following pages:
    - account (personal account) "/account"
    - login (authorization) "/login"
    - news (news) "/news"
    - queue (sign up for service) "/queue"
    - signup (registration) "/signup"
    - faq (frequently asked questions)  "/faq"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
-import React from "react";
+import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom"
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp";
+import Account from "./pages/Account";
+import News from "./pages/News";
+import Queue from "./pages/Queue";
+import FAQ from "./pages/FAQ";
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/login" element={<Login/>}></Route>
+            <Route path="/signup" element={<SignUp/>}></Route>
+            <Route path="/account" element={<Account/>}></Route>
+            <Route path="/news" element={<News/>}></Route>
+            <Route path="/queue" element={<Queue/>}></Route>
+            <Route path="/faq" element={<FAQ/>}></Route>
+            <Route path="/" element={<Navigate replace to="/login"/>} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
