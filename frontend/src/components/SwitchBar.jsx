a762b41930789d4a9f6e5acc49b7331d5b8f07bc
anti-kon <124840747+anti-kon@users.noreply.github.com>
2023-11-10 16:32:16 +0300

    Add switch bar (#19)
    * Add shadow button
    
    -add shadow button layout
    -add shadow button style and animation
    
    * Add switch bar component
    
    - add switch bar layout
    - configure navigation between pages
    
    * Add SwitchBar at pages
    
    - add SwitchBar component at pages: News, Account, FAQ, BulletinBoard, Queue

---
+++ frontend/src/components/SwitchBar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {useNavigate} from "react-router-dom";
+import ShadowButton from "./UI/buttons/shadow_button/ShadowButton";
+import {BiInfoCircle, BiNews} from "react-icons/bi";
+import {TbTag} from "react-icons/tb";
+import {HiPlus} from "react-icons/hi";
+
+const SwitchBar = (props) => {
+    const navigate = useNavigate();
+
+    const iconsStyle = {width: "24px",
+                        height: "auto",
+                        marginRight: "3px",
+                        marginLeft: "-3px"};
+
+    return (
+        <div {...props}>
+            <ShadowButton onClick={() => {navigate('../news');}}>
+                <BiNews style={iconsStyle}/>
+                Новости
+            </ShadowButton>
+            <ShadowButton onClick={() => {navigate('../bulletin_board');}}>
+                <TbTag style={iconsStyle}/>
+                Объявления
+            </ShadowButton>
+            <ShadowButton onClick={() => {navigate('../queue');}}>
+                <HiPlus style={iconsStyle}/>
+                Запись
+            </ShadowButton>
+            <ShadowButton onClick={() => {navigate('../faq');}}>
+                <BiInfoCircle style={iconsStyle}/>
+                Информация
+            </ShadowButton>
+        </div>
+    );
+};
+
+export default SwitchBar;
--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import SwitchBar from "../components/SwitchBar";
 
 const Account = () => {
     return (
         <div>
-
+            <SwitchBar></SwitchBar>
         </div>
     );
 };
