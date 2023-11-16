7e52edf4f3ea97ceb685097a8445321564d9483e
anti-kon <124840747+anti-kon@users.noreply.github.com>
2023-11-16 23:37:32 +0300

    Add header modal (#30)
    * Add header modal
    
    -add header modal layout
    -add header modal styles and animations
    -add navigations to account page and login page from modal
    
    * Change header size
    
    -change header width from 1200px to 960px
    
    * Squashed commit of the following:
    
    commit 8855b94bebbc1708a891c22b33c83201aa3e67dd
    Author: anti-kon <124840747+anti-kon@users.noreply.github.com>
    Date:   Thu Nov 16 23:18:42 2023 +0300
    
        make appointmets page (#29)
    
        - change page name to AppointmentsList ("./appointments")
        - add routes to diffrents appointments (laundry, another, user appointments)
        - add 4 appointments ("Очередь на стирку", "Запись на дезинсекцию", "Запись на ремонт", "Мои записи") to AppointmentsList with styles and animations
    
    commit 0ec9d873a3035c709c22110f8e9badad3da5c6f7
    Author: anti-kon <124840747+anti-kon@users.noreply.github.com>
    Date:   Thu Nov 16 23:17:25 2023 +0300
    
        Make faq page (#28)
    
        * Add tip component
    
        - add tip component layout
        - add tip component styles and animations
    
        * Add tip example
    
        -add to faq page render array of tips

--- frontend/src/components/UI/headers/MajorHeader/MajorHeader.jsx
+++ frontend/src/components/UI/headers/MajorHeader/MajorHeader.jsx
@@ -1,14 +1,66 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import classes from "./MajorHeader.module.css";
 import img from "../../../../img/icon.svg";
-import {BiSolidDownArrow} from "react-icons/bi";
+import {BiExit, BiSolidDownArrow, BiUser} from "react-icons/bi";
+import {useNavigate} from "react-router-dom";
+import ContentBox from "../../content_boxes/content_box/ContentBox";
+import ShadowButton from "../../buttons/shadow_button/ShadowButton";
 
 const MajorHeader = () => {
+    const navigate = useNavigate();
+    
+    const [isModalVisible, setIsModalVisible] = useState(false)
+
+    const modal = useRef(null);
+    const buttonOpenModel = useRef(null);
+
+    useEffect(() => {
+         const onClick = e => {
+             if (modal.current != null &&
+                 !modal.current.contains(e.target) &&
+                 !buttonOpenModel.current.contains(e.target))
+                 setIsModalVisible(false);
+         }
+         document.addEventListener('click', onClick);
+         return () => document.removeEventListener('click', onClick);
+    }, []);
+
+
     return (
         <header className={classes.majorHeader}>
-            <img className={classes.iconStyle} src={img}/>
-            <label className={classes.accountLabel}>Пользователь</label>
-            <BiSolidDownArrow className={classes.arrowStyle} />
+            <div className={classes.headerContent}>
+                <img className={classes.iconStyle} src={img}/>
+                <div ref={buttonOpenModel}
+                     onClick={() => {setIsModalVisible(!isModalVisible);}}
+                     className={classes.accountButton}>
+                    <label className={classes.accountLabel}>Пользователь</label>
+                    <BiSolidDownArrow className={classes.arrowStyle} />
+                </div>
+                {isModalVisible &&
+                    <ContentBox style={{
+                                    maxWidth: "min-content",
+                                    position: "absolute",
+                                    top: "43px",
+                                    right: "7px",
+                                    background: "white"}}>
+                        <div ref={modal} className={classes.modalContent}>
+                            <ShadowButton onClick={() => {navigate("../account");}}
+                                          style={{
+                                              minWidth: "max-content",
+                                              fontSize: "15px",
+                                              padding: "3px 7px 3px 6px"}}>
+                                <BiUser className={classes.accountButtonIcon}/>Личный кабинет
+                            </ShadowButton>
+                            <ShadowButton onClick={() => {navigate("../login")}}
+                                          style={{
+                                              minWidth: "max-content",
+                                              fontSize: "15px",
+                                              padding: "3px 7px 3px 6px"}}>
+                                <BiExit className={classes.exitButtonIcon}/>Выйти
+                            </ShadowButton>
+                        </div>
+                    </ContentBox>}
+            </div>
         </header>
     );
 };
