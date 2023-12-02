25dccf71bc7dc923e6fa4db7aa8c9312ca4cde17
Anton Konopelkin <anti.kon.40@yandex.ru>
2023-12-02 20:47:10 +0300

    add account component
    - add account component layout
    - add account component style

---
+++ frontend/src/components/AccountComponent.jsx
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+import {BiCog, BiSolidDownArrow, BiSolidUpArrow} from "react-icons/bi";
+import ContentBox from "./UI/content_boxes/content_box/ContentBox";
+import FileComponent from "./UI/files/FileComponent";
+import "../styles/Account.css"
+
+const AccountComponent = ({userName, ...props}) => {
+    const [isContentVisible, setIsContentVisible] = useState(false);
+
+    const files = ["квитация за оплату ноябрь 2021.pdf",
+                   "квитация за оплату октябрь 2021.pdf",
+                   "квитация за оплату сентябрь 2021.pdf",
+                   "квитация за оплату август 2021.pdf",
+                   "квитация за оплату июль 2021.pdf",
+                   "квитация за оплату июнь 2021.pdf"];
+
+    const filePost = (filesArray, limit) => {
+        let content = []
+        for (let index = 0; index < limit; index++)
+            content.push(<FileComponent key={index}>{filesArray[index]}</FileComponent>);
+        return content;
+    };
+
+
+    return (
+        <div {...props} className={"account"}>
+            <div className={"label-block"}>
+                <label>{userName}</label>
+                <BiCog
+                    style={{
+                        width: "28px",
+                        height: "auto",
+                        strokeWidth: "0.7px"}}
+                    className={"label-button"} />
+            </div>
+            <ContentBox style={{padding: "15px"}}>
+                <div style={{fontSize: "18px", marginLeft: "0", marginRight: "0"}} className={"label-block"}>
+                    <label>Мои файлы</label>
+                    {isContentVisible ?
+                        <BiSolidUpArrow className={"label-button"}/> :
+                        <BiSolidDownArrow className={"label-button"}/>}
+                </div>
+                <div className={"file-list"}>
+                    {isContentVisible ? filePost(files, files.length) : filePost(files, 4)}
+                </div>
+            </ContentBox>
+            <div className={"drag-and-drop-block"}>
+                Перетащите ваши файлы в эту область или кликните для добавления
+            </div>
+        </div>
+    );
+};
+
+export default AccountComponent;
--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SwitchBar from "../components/SwitchBar";
 import MajorHeader from "../components/UI/headers/MajorHeader/MajorHeader";
 import "../styles/Account.css";
+import AccountComponent from "../components/AccountComponent";
 
 const Account = () => {
     return (
@@ -9,6 +10,7 @@ const Account = () => {
             <MajorHeader></MajorHeader>
             <div className={"account-page"}>
                 <SwitchBar></SwitchBar>
+                <AccountComponent userName={"UserName"} style={{margin: "0 10px 0 20px"}}></AccountComponent>
             </div>
         </div>
     );
