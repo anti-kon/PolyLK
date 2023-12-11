527f8afe75e71b837a9b81faf27d547018bc69d7
anti-kon <124840747+anti-kon@users.noreply.github.com>
2023-12-11 21:53:09 +0300

    Make account page (#37)
    * add file component
    
    - add file component layout
    - add file component style
    
    * add account component
    
    - add account component layout
    - add account component style
    
    * Add account settings
    
    - add account settings layout
    - add account settings style
    
    * add animations
    
    - add animations for buttons at account page
    - add animations for file component
    
    ---------
    
    Co-authored-by: Grebnev Gleb <105592742+Deforc@users.noreply.github.com>

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -1,31 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SwitchBar from "../components/SwitchBar";
 import MajorHeader from "../components/UI/headers/MajorHeader/MajorHeader";
 import "../styles/Account.css";
+import AccountComponent from "../components/AccountComponent";
+import AccountSettingsComponent from "../components/AccountSettingsComponent";
 
-<<<<<<< Updated upstream
-const Account = () => {
-=======
+
 const Account = (props) => {
     const [isEdited, setIsEdited] = useState(false);
 
->>>>>>> Stashed changes
     return (
         <div>
-<<<<<<< Updated upstream
             <MajorHeader></MajorHeader>
             <div className={"account-page"}>
                 <SwitchBar></SwitchBar>
-            </div>
-=======
-<<<<<<< Updated upstream
-            <SwitchBar></SwitchBar>
-=======
-            <MajorHeader></MajorHeader>
-            <div className={"account-page"}>
-                <SwitchBar></SwitchBar>
-<<<<<<< Updated upstream
-=======
                 {
                     !isEdited ?
                         <AccountComponent
@@ -35,16 +23,14 @@ const Account = (props) => {
                         </AccountComponent> :
                         <div className={"settings-block"}>
                             <AccountSettingsComponent
+
                                 person={props.person}
                                 changePerson={props.changePerson}
                                 returnFunc={() => {setIsEdited(false);}}>
                             </AccountSettingsComponent>
                         </div>
                 }
->>>>>>> Stashed changes
             </div>
->>>>>>> Stashed changes
->>>>>>> Stashed changes
         </div>
     );
 };
