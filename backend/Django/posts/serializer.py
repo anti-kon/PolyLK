8da97279d67577bf8d8e8de8da03d3fd21a030e9
Brendow271 <pashashkuropat@yandex.ru>
2023-12-11 22:29:59 +0300

    Added the ability to add photos

--- backend/Django/posts/serializer.py
+++ backend/Django/posts/serializer.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 from .models import Persons, Ads
 
-
 class PostsSerializer(serializers.ModelSerializer):
+
+    list_photo_ads = serializers.ListField(child=serializers.CharField(), write_only=True)
+
     class Meta:
         model = Ads
         fields = '__all__'
+
+    def create(self, validated_data):
+        list_photo_ads_data = validated_data.pop('list_photo_ads', [])
+        ad_instance = Ads.objects.create(**validated_data)
+
+        for address_file in list_photo_ads_data:
+            ad_instance.list_photo_ads.create(address_files=address_file)
+
+        return ad_instance
+
 
--- backend/Django/posts/views.py
+++ backend/Django/posts/views.py
@@ -1,6 +1,7 @@
 import json
 
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.views import APIView
 from .models import Persons, Ads
 from .serializer import PostsSerializer
@@ -10,53 +11,52 @@ from django.db import *
 
 
 class PostsView(APIView):
-    def post(self, request):
+     def post(self, request):
         try:
             serializer = PostsSerializer(data=request.data)
             if serializer.is_valid(raise_exception=True):
                 dorm_num_ads = serializer.validated_data['dorm_num_ads']
                 info_ads = serializer.validated_data['info_ads']
                 price_ads = serializer.validated_data['price_ads']
+
+                if price_ads < 0:
+                    return Response({'error': 'Invalid price'}, status=400)
+
                 alternative_payment_ads = serializer.validated_data['alternative_payment_ads']
-                # list_photo_ads = serializer.validated_data['list_photo_ads'] # разобрать с фотографиями
                 id_person_ads = serializer.validated_data['id_person_ads']
 
                 serializer.save()
+
                 return Response(status=200)
         except DatabaseError:
-            return Response(status=503)
+            return Response({'error': 'Database not responding'}, status=503)
 
-    def get(self, request):
-        try:
-            dorm_num_ads = request.GET.get('dorm_num_ads')
-            info_ads = request.GET.get('info_ads')
-            price_ads = request.GET.get('price_ads')
-            alternative_payment_ads = request.GET.get('alternative_payment_ads')
-            list_photo_ads = request.GET.get('list_photo_ads')
-            id_person_ads = request.GET.get('id_person_ads')
+def get(self, request, *args, **kwargs):
+    dorm_num_ads = self.request.query_params.get('dorm_num_ads')
+    try:
+        queryset = Ads.objects.all()
 
-        # except ObjectDoesNotExist:
-        #     print("Объект не сушествует")
-        # except MultipleObjectsReturned:
-        #     print("Найдено более одного объекта")
+        if dorm_num_ads:
+            queryset = queryset.filter(list_ads__dorm_num_ads=dorm_num_ads)
 
-            return Response(status=200)
-        except DatabaseError:
-            return Response(status=503)
+        serializer = PostsSerializer(queryset, many=True)
+        return Response(serializer.data, status=200)
+    except DatabaseError:
+        return Response(status=503)
 
-    def delete(self, request):
-        try:
-            serializer = PostsSerializer(data=request.data)
-            if serializer.is_valid(raise_exception=True):
-                id_delete = serializer.validated_data['id_ads']
-                id_ads = Ads.objects.filter(id_ads=id_delete)
-                if id_ads:
-                    id_ads.delete()
-                    return Response(status=200)
-                else:
-                    return Response(status=404)
-        except DatabaseError:
-            return Response(status=503)
+def delete(self, request):
+    try:
+        serializer = PostsSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            id_delete = serializer.validated_data['id_ads']
+            id_ads = Ads.objects.filter(id_ads=id_delete)
+            if id_ads:
+                id_ads.delete()
+                return Response(status=200)
+            else:
+                return Response(status=404)
+    except DatabaseError:
+        return Response(status=503)
 
 
 
