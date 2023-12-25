d89e5e2347604506498942ce6c7b3143cd29fc03
Anton Konopelkin <anti.kon.40@yandex.ru>
2023-12-25 20:12:49 +0300

    add news component

--- frontend/src/components/NewsComponent.jsx
+++ frontend/src/components/NewsComponent.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const NewsComponent = () => {
+const NewsComponent = (props) => {
+
+    const maxTextLength = 256;
+
+    const isNeedAdopt = (inputText) => {
+        return inputText.length > maxTextLength;
+    }
+
+    const adoptText = (inputText) => {
+        let lastWordIndex = inputText.slice(0, (maxTextLength + 1)).lastIndexOf(' ');
+        lastWordIndex = lastWordIndex > maxTextLength ? maxTextLength : lastWordIndex;
+        return isNeedAdopt(inputText) ? inputText.slice(0, lastWordIndex) + '...' : inputText;
+    }
+
+    const [text, setTest] = useState(adoptText(props.children));
+    const [isFullTextVisible, setIsFullTextVisible] = useState(false);
+
+    const showButton = isNeedAdopt(props.children);
+
     return (
-        <div>
-            <p>Test</p>
-            <button>Показать ещё</button>
-            <img src="https://www.spbstu.ru/upload/medialibrary/e88/248.jpg"></img>
+        <div className={'news-body'}>
+            <p className={'news-text'}>{text}</p>
+            {showButton && (
+                !isFullTextVisible ?
+                    <button
+                        className={'news-show-more'}
+                        onClick={() => {setTest(props.children);
+                                        setIsFullTextVisible(true)}}>
+                        Показать ещё
+                    </button> :
+                    <button
+                        className={'news-show-more'}
+                        onClick={() => {setTest(adoptText(props.children));
+                                        setIsFullTextVisible(false)}}>
+                        Скрыть
+                    </button>
+            )}
         </div>
     );
 };
