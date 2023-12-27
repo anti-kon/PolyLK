c9c62de34b1a726ec1b55dd04478b87ca7fd9141
anti-kon <124840747+anti-kon@users.noreply.github.com>
2023-12-27 22:36:23 +0300

    Make news page (#40)
    * add news component
    
    * make refaction

--- frontend/src/pages/News.jsx
+++ frontend/src/pages/News.jsx
@@ -5,13 +5,21 @@ import MajorHeader from "../components/UI/headers/MajorHeader/MajorHeader";
 import "../styles/News.css";
 import NewsComponent from "../components/NewsComponent";
 
+const news = [{id: 0, text: "Написание текстов для главных страниц сайта – дело непростое. Проблема в том, что существует сразу несколько подходов к подготовке таких материалов."},
+              {id: 1, text: "Для удобства мы разнесли «главные тексты» по 4 условным категориям. Сразу хотим предупредить, что категории это не обособленные: частенько можно встретить работы, которые вбирают в себя признаки сразу нескольких типов. Также здесь не упоминаются тексты для лендингов, где главная страница – основа основ. Речь пойдет об обычных, «классических» сайтах. Довольно демагогии с нашей стороны, приступаем."}]
+
 const News = () => {
     return (
         <div>
             <MajorHeader></MajorHeader>
             <div className={"news-page"}>
                 <SwitchBar></SwitchBar>
-                <div>
+                <div className={"news-list"}>
+                    {news.map((oneNews) =>
+                        <ContentBox key={oneNews.id} style={{display: "flex", marginBottom: "20px", boxSizing: "border-box"}}>
+                            <NewsComponent>{oneNews.text}</NewsComponent>
+                        </ContentBox>
+                    )}
                 </div>
             </div>
         </div>
