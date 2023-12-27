40598cc55a17f6e741fcf6aba1c989d9d169a6a6
anti-kon <124840747+anti-kon@users.noreply.github.com>
2023-12-27 22:47:22 +0300

    Add tip at sign up page (#42)
    * password styling fix
    
    * select styling fix
    
    * add sign_up tip
    
    - add modal with password tip

--- frontend/src/components/UI/select/Select.jsx
+++ frontend/src/components/UI/select/Select.jsx
@@ -1,14 +1,15 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import classes from "./Seletc.module.css";
 
 const Select = ({...props}) => {
     return (
 
-        <select {...props} className = {classes.select}>
+        <select {...props}
+                className = {classes.select}>
             <option value={""} disabled selected hidden > {props.placeholder} </option>
             {props.options.map(option =>
                 <option value={option.value}>
-                    {option.name}
+                    <label className={classes.option}>{option.name}</label>
                 </option>
             )}
         </select>
