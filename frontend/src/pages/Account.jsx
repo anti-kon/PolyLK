6d47c66843af3766198a19ac3a094f80ed81461b
anti-kon <124840747+anti-kon@users.noreply.github.com>
2023-12-31 01:24:18 +0300

    Complete sign up page (#46)
    * Remake select component
    
    * Add app context
    
    * Add service page
    
    * configure http requests
    
    * Add button state
    
    - prevent unnecessary requests to the server

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -1,13 +1,15 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import SwitchBar from "../components/SwitchBar";
 import MajorHeader from "../components/UI/headers/MajorHeader/MajorHeader";
 import "../styles/Account.css";
 import AccountComponent from "../components/AccountComponent";
 import AccountSettingsComponent from "../components/AccountSettingsComponent";
+import { PersonContext } from '../App';
 
 
 const Account = (props) => {
     const [isEdited, setIsEdited] = useState(false);
+    const { person, setPerson } = useContext(PersonContext);
 
     return (
         <div>
@@ -17,14 +19,14 @@ const Account = (props) => {
                 {
                     !isEdited ?
                         <AccountComponent
-                            person={props.person}
+                            person={person}
                             gearOnClick={() => {setIsEdited(true);}}
                             style={{margin: "7px 10px 0 20px"}}>
                         </AccountComponent> :
                         <div className={"settings-block"}>
                             <AccountSettingsComponent
-                                person={props.person}
-                                changePerson={props.changePerson}
+                                person={person}
+                                changePerson={setPerson}
                                 returnFunc={() => {setIsEdited(false);}}>
                             </AccountSettingsComponent>
                         </div>
