5b2b49b00b22c95a9532a95176bd96d80f1265f1
Anton Konopelkin <anti.kon.40@yandex.ru>
2023-12-30 17:14:06 +0300

    Remake select component

--- frontend/src/components/UI/select/Select.jsx
+++ frontend/src/components/UI/select/Select.jsx
@@ -1,18 +1,50 @@
 import React, {useEffect, useState} from 'react';
-import classes from "./Seletc.module.css";
+import classes from "./Select.module.css";
+import {BiSolidDownArrow} from "react-icons/bi";
 
-const Select = ({...props}) => {
+const Select = ({valid = true, ...props}) => {
+    const [isListVisible,setIsListVisible] = useState(false);
+    const [value, setValue] = useState("");
+
+    const handleBlur = (e) => {
+        const currentTarget = e.currentTarget;
+
+        requestAnimationFrame(() => {
+            setIsListVisible(currentTarget.contains(document.activeElement))
+        });
+    };
+
     return (
-
-        <select {...props}
-                className = {classes.select}>
-            <option value={""} disabled selected hidden > {props.placeholder} </option>
-            {props.options.map(option =>
-                <option value={option.value}>
-                    <label className={classes.option}>{option.name}</label>
-                </option>
-            )}
-        </select>
+        <div
+            onFocus={() => setIsListVisible(true)}
+            onBlur={handleBlur}>
+            <div {...props} className={valid ? classes.selectBody : classes.selectBodyError}>
+                {value === "" ? props.placeholder : <label className={classes.selectValue}>{value}</label>}
+                <button className={classes.selectClick}>
+                    <BiSolidDownArrow className={valid ? classes.selectArrow : classes.selectArrowError}/>
+                </button>
+            </div>
+            {
+                isListVisible &&
+                <div className={valid ? classes.selectListModal : classes.selectListModalError}>
+                    <select
+                        size={6}
+                        className={valid ? classes.selectList : classes.selectListError}
+                        onChange={e => {
+                            setIsListVisible(false);
+                            setValue(e.target.options[e.target.selectedIndex].text);
+                            props.onChange !== undefined && props.onChange(e);
+                        }}>
+                        {props.options.map(option =>
+                            <option
+                                key={option.key}
+                                className={classes.selectOption}
+                                value={option.value}>{option.name}</option>
+                        )}
+                    </select>
+                </div>
+            }
+        </div>
     );
 };
 
