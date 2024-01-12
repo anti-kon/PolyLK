7599da41ea7ee424e9a6d24817b4b9412604b8c8
Вячеслав Карасев <115403834+slavikfish03@users.noreply.github.com>
2024-01-12 05:16:39 +0300

    http for infoperson #3

--- backend/Django/infoPerson/views.py
+++ backend/Django/infoPerson/views.py
@@ -116,42 +116,5 @@ class PersonsDocsView(APIView):
                 serializer.save()
                 return Response(data='OK', status=200)
 
-
-            # self.convert_bytes_to_file(id_person_doc, name_file, file)
-            #
-            # login_person = Persons.objects.filter(id_person=id_person_doc).values_list('login_person', flat=True).first()
-            # path_to_doc = 'infoPerson\\docs'
-            # data = {
-            #     "id_person_doc": id_person_doc,
-            #     "name_doc": name_file,
-            #     "path_to_doc": f'{path_to_doc}\\{name_file}\\{login_person}_{name_file}.pdf'
-            # }
-            #
-            # serializer = PersonsDocsSerializer(data=data)
-            # if serializer.is_valid(raise_exception=True):
-            #     serializer.save()
-            #     return Response(data='OK', status=200)
         except DatabaseError:
             return Response(data='Database Error', status=503)
-
-    # def convert_bytes_to_file(self, id_person_doc, name_file, file):
-    #     file_bytes = bytes.fromhex(file.replace("0x", ""))
-    #     file_pdf_buffer = BytesIO()
-    #     file_pdf_canvas = canvas.Canvas(file_pdf_buffer)
-    #
-    #     file_pdf_canvas.drawString(100, 750, file_bytes.decode("utf-8"))
-    #     file_pdf_canvas.save()
-    #
-    #     login_person = Persons.objects.filter(id_person=id_person_doc).values_list('login_person', flat=True).first()
-    #
-    #     path_to_doc = 'infoPerson\\docs'
-    #     full_path = f'{os.getcwd()}\\{path_to_doc}'
-    #
-    #     list_directory = os.listdir(full_path)  # need for folder with name_doc
-    #
-    #     if name_file not in list_directory:
-    #         os.mkdir(f'{full_path}\\{name_file}')
-    #     full_path = f'{full_path}\\{name_file}'
-    #
-    #     with open(f'{full_path}\\{login_person}_{name_file}.pdf', 'wb') as f:
-    #         f.write(file_pdf_buffer.getvalue())
