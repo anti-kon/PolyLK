92f00289ac4c1591d1b31e41a0031320567c40f9
Brendow271 <pashashkuropat@yandex.ru>
2024-01-12 03:16:32 +0300

    Update

--- backend/Django/posts/models.py
+++ backend/Django/posts/models.py
@@ -6,6 +6,8 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from rest_framework import serializers
 
 class Ads(models.Model):
     id_ads = models.AutoField(primary_key=True)
