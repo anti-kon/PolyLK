520b903288b049044857f9135c20330465e164ea
Brendow271 <pashashkuropat@yandex.ru>
2024-03-31 20:23:19 +0300

    Update JWT authorization
    Fix LKD-39
    
    - response authorization
    
    Partial solve LKD-23
    
    - update news

--- microservices/authorization/core/views.py
+++ microservices/authorization/core/views.py
@@ -6,6 +6,15 @@ from django.db import *
 from .models import Persons
 from .serializer import AuthorizationSerializer
 import jwt
+import json
+import jwt
+import logging
+from django.http import HttpResponse
+from django.utils.deprecation import MiddlewareMixin
+from rest_framework.response import Response
+
+
+SECRET_KEY = "PolyLK"
 
 
 class AuthorizationView(APIView):
@@ -33,3 +42,49 @@ class AuthorizationView(APIView):
         except DatabaseError:
             return Response('База данных не отвечает', status=503)
 
+class CustomMiddleware(APIView):
+
+    """
+    Custom Middleware Class to process a request before it reached the endpoint
+    """
+
+    def post(self, request):
+
+        """
+        Custom middleware handler to check authentication for a user with JWT authentication
+        :param request: Request header containing authorization tokens
+        :type request: Django Request Object
+        :return: HTTP Response if authorization fails, else None
+        """
+        print(request)
+        jwt_token = request.data.get('token', None)
+        print(1)
+        print(jwt_token)
+        # return None
+        # logger.info(f"request received for endpoint {str(request.path)}")
+
+        # If token Exists
+        if jwt_token:
+            try:
+                print(2)
+                payload = jwt.decode(jwt_token.split(' ')[1], SECRET_KEY, algorithms=['HS256'])
+                id_person = payload['id_person']
+                login_person = payload['login_person']
+                print(payload)
+                # logger.info(f"Request received from user - {userid}, company - {company_id}")
+                return Response('OK', status=200)
+            except jwt.ExpiredSignatureError:
+                print(3)
+                return Response('Истек срок токена', status=401)  # посмотреть код ошибки
+            except (jwt.DecodeError, jwt.InvalidTokenError):
+                print(4)
+                return Response('Неверный токен', status=401)
+        else:
+            # response = create_response(
+            #     "", 401, {"message": "Authorization not found, Please send valid token in headers"}
+            # )
+            # #logger.info(f"Response {response}")
+            # return HttpResponse(json.dumps(response), status=401)
+
+            print(5)
+            return Response('Пользователь не авторизован', status=401)
--- microservices/news/news/middleware.py
+++ microservices/news/news/middleware.py
@@ -1,85 +1,29 @@
-"""
-File in which we have the middleware for Django for Authenticating API requests
-"""
 import json
 import jwt
 import logging
+
+import requests
 from environs import Env
 from django.http import HttpResponse
 from django.utils.deprecation import MiddlewareMixin
 from rest_framework.response import Response
+class AuthorizationMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
 
-# # Initialize logger
-# logger = logging.getLogger(__name__)
-
-# Get JWT secret key
-env = Env()
-env.read_env()
-# SECRET_KEY = env("PolyLK")
-SECRET_KEY = "PolyLK"
-
-
-# def create_response(request_id, code, message):
-#
-#     """
-#     Function to create a response to be sent back via the API
-#     :param request_id:Id fo the request
-#     :param code:Error Code to be used
-#     :param message:Message to be sent via the APi
-#     :return:Dict with the above given params
-#     """
-#
-#     try:
-#         req = str(request_id)
-#         data = {"data": message, "code": int(code), "request_id": req}
-#         return data
-#     except Exception as creation_error:
-#         logger.error(f'create_response:{creation_error}')
-
-
-class CustomMiddleware(MiddlewareMixin):
-
-    """
-    Custom Middleware Class to process a request before it reached the endpoint
-    """
-
-    def process_request(self, request):
-
-        """
-        Custom middleware handler to check authentication for a user with JWT authentication
-        :param request: Request header containing authorization tokens
-        :type request: Django Request Object
-        :return: HTTP Response if authorization fails, else None
-        """
-
-        jwt_token = request.headers.get('authorization', None)
+    def __call__(self, request):
+        token = request.META.get('HTTP_AUTHORIZATION', None)
         print(1)
-        print(jwt_token)
-        # return None
-        #logger.info(f"request received for endpoint {str(request.path)}")
-
-        #If token Exists
-        if jwt_token:
-            try:
-                print(2)
-                payload = jwt.decode(jwt_token.split(' ')[1], SECRET_KEY, algorithms=['HS256'])
-                id_person = payload['id_person']
-                login_person = payload['login_person']
-                print(payload)
-                #logger.info(f"Request received from user - {userid}, company - {company_id}")
-                return None
-            except jwt.ExpiredSignatureError:
+        print(token)
+        if token:
+            response = requests.post('http://localhost:8000/verify', data={'token': token})
+            print(response)
+            print(2)
+            if response.status_code == 200:
                 print(3)
-                return Response('Истек срок токена', status=401)# посмотреть код ошибки
-            except (jwt.DecodeError, jwt.InvalidTokenError):
+                return None
+            else:
                 print(4)
-                return Response('Неверный токен', status=401)
-        else:
-            # response = create_response(
-            #     "", 401, {"message": "Authorization not found, Please send valid token in headers"}
-            # )
-            # #logger.info(f"Response {response}")
-            # return HttpResponse(json.dumps(response), status=401)
-            print(5)
-            return Response('Пользователь не авторизован', status=401)
-
+                return HttpResponse(response, status=response.status_code)
+        print(5)
+        return HttpResponse('Токен не найден', status=401)
