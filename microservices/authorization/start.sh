6493aacc3b19e3fc6c12dc54598797a3eb613077
Konopelkin Anton <anti.kon.40@yandex.ru>
2024-04-13 16:10:15 +0300

    Add daemon flag (#118)
    - add daemon flag in start.sh file

--- microservices/authorization/start.sh
+++ microservices/authorization/start.sh
@@ -1,4 +1,4 @@
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
-gunicorn authorization.wsgi:application --bind 0.0.0.0:8000
+gunicorn authorization.wsgi:application --bind 0.0.0.0:8000 --daemon
--- microservices/registration/start.sh
+++ microservices/registration/start.sh
@@ -1,4 +1,4 @@
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
-gunicorn registration.wsgi:application --bind 0.0.0.0:8000
+gunicorn registration.wsgi:application --bind 0.0.0.0:8000 --daemon
